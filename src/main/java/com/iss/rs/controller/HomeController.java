d0e97347383728c3eefc3f31f9e180e2f8cd1fa4
changhe.a.ye <ychcnm@gmail.com>
2019-04-09 16:44:35 +0800

    integrated with mybatis

--- src/main/java/com/iss/rs/controller/HomeController.java
+++ src/main/java/com/iss/rs/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.iss.rs.controller;
 
+import com.iss.rs.entity.Machine;
+import com.iss.rs.service.MachineService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
@@ -11,12 +16,16 @@ public class HomeController {
     //添加一个日志器
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+    @Resource(name = "MacService")
+    private MachineService machineService;
+
     //映射一个action
     @RequestMapping("/index")
     public String index() throws Exception {
         //输出日志文件
         logger.info("the first jsp pages");
         //返回一个index.jsp这个视图
+        List<Machine> m = machineService.getAllMachine();
         return "index";
     }
 }
