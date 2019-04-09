d264e886ea3ca19404b01c3b2ab8492b75d267fa
changhe.a.ye <ychcnm@gmail.com>
2019-04-09 18:07:38 +0800

    integrated with mybatis and format

--- src/main/java/com/iss/rs/service/MachineService.java
+++ src/main/java/com/iss/rs/service/MachineService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
 
-@Service(value="MacService")
+@Service(value = "MacService")
 public class MachineService {
 
     @Resource
